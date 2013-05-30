938a1ff64802e604f10ad859def6d00767a80de3
David Smith <codeactual@gmail.com>
2013-05-30 16:02:37 -0700

    chore(grunt): Migrate to grunt-horde

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,13 @@
 module.exports = function(grunt) {
   'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-
-  var mochaShelljsOpt = {stdout: true, stderr: false};
-
-  grunt.initConfig({
-    jshint: {
-      src: {
-        files: {
-          src: ['index.js', 'lib/**/*.js', '!lib/jquery.js']
-        }
-      },
-      grunt: {
-        files: {
-          src: ['Gruntfile.js']
-        }
-      },
-      tests: {
-        options: {
-          expr: true
-        },
-        files: {
-          src: ['test/**/*.js']
-        }
-      },
-      json: {
-        files: {
-          src: ['*.json']
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        options: {
-          compress: false,
-          mangle: false,
-          beautify: true
-        },
-        files: {
-          'dist/sinon-doublist.js': 'dist/sinon-doublist.js'
-        }
-      }
-    },
-    shell: {
-      options: {
-        failOnError: true,
-        stdout: true,
-        stderr: true
-      },
-      build: {
-        command: 'component install --dev && component build --standalone sinonDoublist --name sinon-doublist --out dist --dev'
-      },
-      dist: {
-        command: 'component build --standalone sinonDoublist --name sinon-doublist --out dist'
-      },
-      test_lib: {
-        options: mochaShelljsOpt,
-        command: 'mocha --colors --async-only --recursive --reporter spec test/lib'
-      },
-      test_mocha: {
-        options: mochaShelljsOpt,
-        command: 'mocha --colors --async-only --reporter spec test/mocha.js'
-      },
-      dox_lib: {
-        command: 'apidox --input lib/sinon-doublist/index.js --output docs/sinon-doublist.md'
-      }
-    }
-  });
-
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('dox', ['shell:dox_lib']);
-  grunt.registerTask('build', ['default', 'shell:build']);
-  grunt.registerTask('dist', ['default', 'shell:dist', 'uglify:dist', 'dox']);
-  grunt.registerTask('test', ['build', 'shell:test_lib', 'shell:test_mocha']);
+  require('grunt-horde')
+    .create(grunt)
+    .demand('projName', 'sinon-doublist')
+    .demand('instanceName', 'sinonDoublist')
+    .demand('klassName', 'sinonDoublist')
+    .loot('node-component-grunt')
+    .loot('node-lib-grunt')
+    .loot('./config/grunt')
+    .attack();
 };
