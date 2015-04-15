8482ba7b690784876e4536ccb1ab3ca696f1f460
David Smith <codeactual@gmail.com>
2015-04-15 02:18:04 +0000

    test(node env): run all tests

--- config/grunt/registerTask.js
+++ config/grunt/registerTask.js
@@ -3,5 +3,6 @@ module.exports = function exports() {
 
   const config = {};
   config.default = [this.learn('registerTask.default')[0].concat('shell:dist')];
+  config.test = [this.learn('registerTask.test')[0].concat('shell:test_mocha_adapter')];
   return config;
 };
